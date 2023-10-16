0163c1dcf76bd0cded76c98c439967727f6c4366
ValeraRishniak <128408270+ValeraRishniak@users.noreply.github.com>
2023-10-16 21:15:40 +0300

    Add files via upload

---
+++ P2/hw_project/quotes_toscrape/README.md
@@ -0,0 +1,21 @@
+Реалізація проекту Django
+
+Для роботи проекта необхідний файл /quotes_toscrape/.env зі змінними оточення. Створіть його з таким вмістом і підставте свої значення.
+
+SECRET_KEY=
+
+DATABASE_NAME=
+DATABASE_USER=
+DATABASE_PASSWORD=
+DATABASE_HOST=
+DATABASE_PORT=
+
+EMAIL_HOST=
+EMAIL_PORT=
+EMAIL_HOST_USER=
+EMAIL_HOST_PASSWORD=
+
+Запуск застосунку
+
+cd quotes_toscrape
+py manage.py runserver
---
+++ P2/hw_project/quotes_toscrape/utils/migrate.py
@@ -0,0 +1,56 @@
+import os
+import django
+
+from pymongo import MongoClient
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "quotes_toscrape.settings")
+django.setup()
+
+
+from quotesapp.models import Author, Quote, Tag
+
+import configparser
+
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+mongo_user = config.get('DB', 'user')
+mongodb_pass = config.get('DB', 'pass')
+db_name = config.get('DB', 'db_name')
+domain = config.get('DB', 'domain')
+
+
+
+client = MongoClient(f"mongodb+srv://{mongo_user}:{mongodb_pass}@{domain}/")
+
+db = client.hometask10
+
+
+authors = db.authors.find()
+for author in authors:
+    Author.objects.get_or_create(
+        fullname=author["fullname"],
+        born_date=author["born_date"],
+        born_location=author["born_location"],
+        bio=author["bio"]
+    )
+
+quotes = db.quotes.find()
+
+for quote in quotes:
+    tags = list()
+    for tag in quote['tags']:
+        t, *_ = Tag.objects.get_or_create(name=tag)
+        tags.append(t)
+
+    exists_quote = bool(len(Quote.objects.filter(quote=quote["quote"])))
+
+    if not exists_quote:
+        author = db.authors.find_one({"_id": quote["author"]})
+        a = Author.objects.get(fullname=author["fullname"])
+        q = Quote.objects.create(quote=quote["quote"], author=a)
+
+        for tag in tags:
+            q.tags.add(tag)
+        
